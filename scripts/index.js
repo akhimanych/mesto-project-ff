b0bf611d3f454f94f38839987b3d4e0945595dcc
akhimanych <akhimanych@gmail.com>
2025-05-18 22:40:34 +0300

    first review

--- scripts/index.js
+++ scripts/index.js
@@ -3,13 +3,24 @@ const cardList = document.querySelector(".places__list");
 
 /*Создания карточек*/
 function createCard(element) {
-  const cardElement = cardTemplate.cloneNode(true);
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  if (!cardElement) {
+    console.error("Элемент .card не найден в шаблоне");
+    return null;
+  }
 
   const cardImage = cardElement.querySelector(".card__image");
   const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  if (!cardImage || !cardTitle || !deleteButton) {
+    console.error("Ошибка в структуре шаблона карточки");
+    return null;
+  }
 
   cardImage.src = element.link;
-  cardImage.alt = `Изображение места: ${element.name}`;
+  cardImage.alt = `Пейзажи разных регионов России: ${element.name}`;
   cardTitle.textContent = element.name;
 
   return cardElement;
@@ -24,7 +35,10 @@ cardList.addEventListener("click", (event) => {
 
 /*Добавление начальных карточек*/
 initialCards.forEach((element) => {
-  const cardElement = createCard(element);
+  const cardElement = createCard(element, () => {
+    cardElement.remove();
+  });
+
   if (cardElement) {
     cardList.append(cardElement);
   }
